d34b5445d1bfaa17b7ec20c9cdcffd509cc00810
Fawzi Mouelhi <mouelhiam23@gmail.com>
2025-05-24 10:49:00 +0100

    done

--- Script/game.js
+++ Script/game.js
@@ -28,21 +28,29 @@ spanElement.textContent=masked;
 
 
 var submitbtn=document.querySelector('.submit');
-submitbtn.addEventListener('click',function(){
+submitbtn.addEventListener('click',function(e){
+e.preventDefault()
     //getting the value from input field
 var input=document.getElementById('guessed-letter').value.toLowerCase();
+var isCorrect=false;
 //checking if the guessed letter is correct
 for(var i=0;i<secretWord.length;i++){
-    var temp='';
+    
     //check if the letter is in secretWord
     if(secretWord[i]===input){
         //update the array we created with masked word
         maskedword[i]=input;
-        //join the array back into a string 
-        temp= maskedword.join('');
-        //updating the UI
-        spanElement.textContent=temp
+        isCorrect=true;     
     }}
+
+        if(isCorrect ===true){
+        //updating the UI
+        spanElement.textContent=maskedword.join('');
+        }
+        else{
+            alert('Wrong guess')
+        }
+   
 //clear the input field
 document.getElementById('guessed-letter').value = '';
 })
@@ -50,9 +58,10 @@ document.getElementById('guessed-letter').value = '';
 //select hint button
 var hintbtn=document.querySelector('#hint-text');
 //add event listener to hint button
-hintbtn.addEventListener('click',function(){
+hintbtn.addEventListener('click',function(e){
+    e.preventDefault();
     //get the hint from the list
     var hint=words[wordIndex].hint;
     //updating the UI
-    document.querySelector('.given-hing').textContent=hint;
+    document.querySelector('.given-hint').textContent=hint;
 })
